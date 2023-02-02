2b7cd4d06f3300c9d21ee6d32715692e7dd2a2d1
Tim Chang <zhaotingchang@gmail.com>
2023-02-02 13:12:05 -0800

    rename tests

--- rules/cmd/cmd.spec.js
+++ rules/cmd/cmd.spec.js
@@ -1,12 +1,12 @@
 const cmd = require("./cmd");
 
-describe("when provided exec fails", () => {
+describe("when provided command cannot be found", () => {
   it("returns false", () => {
     expect(cmd({exec: "foobar"})).toBe(false);
   });
 });
 
-describe("when provided exec fails", () => {
+describe("when provided can be found", () => {
   it("returns true", () => {
     expect(cmd({exec: "ls"})).toBe(true);
   });
