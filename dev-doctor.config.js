c8f7a4f0cd4eae535116e0f1b793d8b3fba1a37a
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-02-03 18:07:06 -0600

    Remove non-functional config from local checks

--- dev-doctor.config.js
+++ dev-doctor.config.js
@@ -8,9 +8,4 @@ module.exports = [
     exec: "npm",
     suggestion: "Make sure to install node locally: brew install node",
   },
-  {
-    type: "version",
-    exec: "path/to/executable -v",
-    range: "<1.0.0", // using semver range patterns
-  },
 ];
